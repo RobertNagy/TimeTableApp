16e33f9acd07f27227c695f71dacac09bcb2a386
Robert <robert84n@gmail.com>
2012-05-04 14:58:39 +0200

    i done this biatch
    bla bla

---
+++ timetableapp/timetableapp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  timetableapp
+//
+//  Created by Student vid Yrkeshögskola C3L on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hej walter");
+        
+    }
+    return 0;
+}
+
